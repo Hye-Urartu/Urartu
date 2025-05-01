c47351ff1b518a050dd3faac312569480b5c487f
Joseph Maldjian <josephavm@icloud.com>
2025-05-01 18:41:28 -0500

    Implement Logout Logic

--- app/oidc/logout/route.ts
+++ app/oidc/logout/route.ts
@@ -12,5 +12,17 @@ export async function GET(request: Request) {
       },
     },
   });
-  console.log(session);
+  await prisma.session.delete({
+    where: {
+      id: session?.id,
+    },
+  });
+
+  const response = new Response(null, {
+    status: 302,
+    headers: {
+      location: searchParams.get("post_logout_redirect_uri") as string,
+    },
+  });
+  return response;
 }
