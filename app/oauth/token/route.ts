8be7a585bb1cc301dbdb0ec3e80a81a921304d31
Joseph Maldjian <josephavm@icloud.com>
2025-05-02 01:14:10 -0500

    Add token refreshing support

--- app/oauth/token/route.ts
+++ app/oauth/token/route.ts
@@ -177,4 +177,152 @@ export async function POST(request: NextRequest) {
       }
     );
   }
+  if (body.get("grant_type") === "refresh_token") {
+    const session = await prisma.session.findFirst({
+      where: {
+        tokens: {
+          some: {
+            token: body.get("refresh_token") as string,
+          },
+        },
+      },
+      include: {
+        user: {
+          omit: {
+            password: true,
+          },
+        },
+      },
+    });
+    if (!session) return new Response("Invalid refresh token", { status: 400 });
+    const ks = ((await prisma.jWK.findFirst({})) as jwkPrisma).raw;
+    const keyStore = await jose.JWK.asKeyStore(ks);
+    const [key] = keyStore.all({ use: "sig" });
+    const access_token = await jose.JWS.createSign(
+      {
+        compact: true,
+        //jwk: key,
+        fields: {
+          typ: "jwt",
+        },
+      },
+      key
+    )
+      .update(
+        JSON.stringify({
+          sub: session?.userId,
+          iss: process.env.URI + "/auth",
+          aud: body.get("client_id") as string,
+          exp: DateTime.now().plus({ minutes: 5 }).toSeconds(),
+          iat: DateTime.now().toSeconds(),
+        })
+      )
+      .final();
+    const id_token = await jose.JWS.createSign(
+      {
+        compact: true,
+        //jwk: key,
+        fields: {
+          typ: "jwt",
+        },
+      },
+      key
+    )
+      .update(
+        JSON.stringify({
+          exp: DateTime.now()
+            .plus({ minutes: 1 * 60 })
+            .toSeconds(),
+          sub: session?.userId,
+          iss: process.env.URI + "/auth",
+          ...session.user,
+        })
+      )
+      .final();
+    const refresh_token = await jose.JWS.createSign(
+      {
+        compact: true,
+        //jwk: key,
+        fields: {
+          typ: "jwt",
+        },
+      },
+      key
+    )
+      .update(
+        JSON.stringify({
+          sub: session?.userId,
+          iss: process.env.URI + "/auth",
+          aud: body.get("client_id") as string,
+          exp: DateTime.now()
+            .plus({ minutes: 1 * 60 })
+            .toSeconds(),
+          iat: DateTime.now().toSeconds(),
+        })
+      )
+      .final();
+    await prisma.token.deleteMany({
+      where: {
+        sessionId: session.id,
+        type: "refresh_token",
+      },
+    });
+    await prisma.token.create({
+      data: {
+        expireAt: DateTime.now().plus({ minutes: 60 }).toUTC().toString(),
+        // @ts-ignore
+        token: refresh_token,
+        type: "refresh_token",
+        sessionId: session.id,
+      },
+    });
+
+    await prisma.token.deleteMany({
+      where: {
+        sessionId: session.id,
+        type: "access_token",
+      },
+    });
+    await prisma.token.create({
+      data: {
+        expireAt: DateTime.now().plus({ minutes: 5 }).toUTC().toString(),
+        // @ts-ignore
+        token: access_token,
+        type: "access_token",
+        sessionId: session.id,
+      },
+    });
+    await prisma.token.deleteMany({
+      where: {
+        sessionId: session.id,
+        type: "id_token",
+      },
+    });
+    await prisma.token.create({
+      data: {
+        expireAt: DateTime.now().plus({ days: 30 }).toUTC().toString(),
+        // @ts-ignore
+        token: id_token,
+        type: "id_token",
+        sessionId: session.id,
+      },
+    });
+
+    return new Response(
+      JSON.stringify({
+        access_token,
+        id_token,
+        refresh_token,
+        token_type: "bearer",
+        expires_in: 300,
+        refresh_expires_in: 3600,
+      }),
+      {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
 }
